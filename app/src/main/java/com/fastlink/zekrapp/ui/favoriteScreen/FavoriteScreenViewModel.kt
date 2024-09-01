a78685cf1dec4ce1a52707662f9f57829f83c7b0
bilal Seerwan <ensw02121@student.su.edu.krd>
2024-09-01 11:55:53 +0300

    Removing unnecessary imports and eliminating the unnecessary context parameter in FavoriteScreenViewModel

--- app/src/main/java/com/fastlink/zekrapp/ui/favoriteScreen/FavoriteScreenViewModel.kt
+++ app/src/main/java/com/fastlink/zekrapp/ui/favoriteScreen/FavoriteScreenViewModel.kt
@@ -1,18 +1,15 @@
 package com.fastlink.zekrapp.ui.favoriteScreen
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.fastlink.zekrapp.di.ZekirCategorySingleton
 import com.fastlink.zekrapp.appData.model.ZekirCategoryModel
 import com.fastlink.zekrapp.ui.utils.ZekirCategoryViewModelInterface
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 @HiltViewModel
 class FavoriteScreenViewModel @Inject constructor(
     private val zekirCategorySingleton: ZekirCategorySingleton,
-    @ApplicationContext private val context: Context
 ) : ViewModel(), ZekirCategoryViewModelInterface {
 
     fun getFavoriteZekirCategories(): List<ZekirCategoryModel> =
@@ -20,5 +17,4 @@ class FavoriteScreenViewModel @Inject constructor(
 
     override fun updateZekirCategory(categoryId: Int, isFavorite: Boolean) =
         zekirCategorySingleton.updateCategory(categoryId, isFavorite)
-
 }
